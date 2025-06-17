7105e28bce7fb8501f7300c17bccdbd91ec592da
Jorge Tutor <jorgetutor@gmail.com>
2025-06-17 18:37:24 +0200

    feat: migrate styles to tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jorgetutor.net',
-	integrations: [
-		mdx(),
-		sitemap(),
-	],
+        integrations: [
+                mdx(),
+                sitemap(),
+                tailwind(),
+        ],
 });
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{astro,js,jsx,ts,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
